b075f46b268057a36e87df4f98cde96e085dff01
seangleng <seangleng507@gmail.com>
2024-04-11 11:47:59 +0700

    config enviroment to production

--- src/app/service/baseUrl.service.ts
+++ src/app/service/baseUrl.service.ts
@@ -2,6 +2,6 @@ export const baseURL = () => {
   if (process.env.NODE_ENV === "development") {
     return process.env.NEXT_PUBLIC_DEVELOPMENT_URL;
   } else {
-    return process.env.NEXT_PUBLIC_DEVELOPMENT_URL;
+    return process.env.NEXT_PUBLIC_PRODUCTION_URL;
   }
 };
